42430663bb609f1ee8f0f0d4c236aa314b35b62d
xuzewei <xuzewei_2012@126.com>
2017-05-15 14:50:39 +0800

    WebUiEngineCookies.java解码中文cookie，需要两次解码！！

--- src/main/java/com/github/xuzw/ui_engine_runtime_http_wrapper/cookie/WebUiEngineCookies.java
+++ src/main/java/com/github/xuzw/ui_engine_runtime_http_wrapper/cookie/WebUiEngineCookies.java
@@ -1,9 +1,12 @@
 package com.github.xuzw.ui_engine_runtime_http_wrapper.cookie;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 import javax.servlet.http.Cookie;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.xuzw.ui_engine_runtime.div.location.Location;
 import com.github.xuzw.ui_engine_runtime.event.Event;
@@ -15,25 +18,31 @@ import com.github.xuzw.ui_engine_runtime.input.Inputs;
  * @time 2017年5月14日 下午9:34:46
  */
 public class WebUiEngineCookies {
+    private static final Logger log = LoggerFactory.getLogger(WebUiEngineCookies.class);
     public static final String key_event_type = "uiEngine.event.type";
     public static final String key_event_location_className = "uiEngine.event.location.className";
     public static final String key_event_location_id = "uiEngine.event.location.id";
     public static final String key_prefix_input = "uiEngine.input.";
     private Cookie[] rawCookies;
-    private List<Cookie> inputs = new ArrayList<>();
+    private Inputs inputs = new Inputs();
     private Cookie eventType;
 
     public WebUiEngineCookies(Cookie[] cookies) {
         this.rawCookies = cookies;
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(key_event_type)) {
-                    eventType = cookie;
-                }
-                if (cookie.getName().startsWith(key_prefix_input)) {
-                    inputs.add(cookie);
+        try {
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals(key_event_type)) {
+                        eventType = cookie;
+                    }
+                    if (cookie.getName().startsWith(key_prefix_input)) {
+                        String decodeValue = URLDecoder.decode(URLDecoder.decode(cookie.getValue(), "utf-8"), "utf-8");
+                        inputs.put(cookie.getName().substring(key_prefix_input.length()), decodeValue);
+                    }
                 }
             }
+        } catch (UnsupportedEncodingException e) {
+            log.error("", e);
         }
     }
 
@@ -47,7 +56,6 @@ public class WebUiEngineCookies {
     }
 
     public Event getEvent() {
-        Inputs inputs = getInputs();
         if (eventType == null) {
             Event event = new RefreshEvent();
             event.setInputs(inputs);
@@ -67,13 +75,5 @@ public class WebUiEngineCookies {
 
     public Location getEventLocation() {
         return new Location(getCookie(key_event_location_className).getValue(), getCookie(key_event_location_id).getValue());
-    }
-
-    public Inputs getInputs() {
-        Inputs inputs = new Inputs();
-        for (Cookie cookie : this.inputs) {
-            inputs.put(cookie.getName().substring(key_prefix_input.length()), cookie.getValue());
-        }
-        return inputs;
     }
 }
